52e74ffc7ebb68e0d4f7c0742a358f38debc2bfa
ThereWolf <bdunham484@gmail.com>
2022-06-29 15:42:20 -0500

    add inquirer/first question

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-// import inquirer from 'inquirer';
-// console.log(inquirer);
+import inquirer from 'inquirer';
+// import { writeFile } from 'fs';
+// import { generatePage } from './src/page-template.js';
 
-import { writeFile } from 'fs';
+// const pageHTML = generatePage(userName, githubName);
 
-const profileDataARgs = process.argv.slice(2, process.argv.length);
-const [userName, githubName] = profileDataARgs;
+// writeFile('./index.html', pageHTML, err => {
+//     if (err) throw  err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-import { generatePage } from './src/page-template.js';
-
-// const generatePage = (userName, githubName) => {
-//     return `
-//     <!DOCTYPE html>
-//     <html lang="en">
-//         <head>
-//             <meta charset="UTF-8">
-//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//             <meta http-eqiv="X=UA-Compatible" content="ie=edge">
-//             <title>Portfolio</title>
-//         </head>
-
-//         <body>
-//             <h1>${userName}</h1>
-//             <h2><a href="https://github.com/${githubName}">Github</a></h2>
-//         </body>
-//     </html>
-//     `;
-// };
-
-
-
-
-
-
-
-writeFile('./index.html', generatePage(userName, githubName), err => {
-    if (err) throw  err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
