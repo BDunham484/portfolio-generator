3ff5786b0799928123d7b751b198dc318b2cf342
ThereWolf <bdunham484@gmail.com>
2022-06-30 15:17:08 -0500

    create header/footer

--- src/page-template.js
+++ src/page-template.js
@@ -25,7 +25,13 @@
 
 
 
-export const generatePage = (userName, githubName) => {
+export const generatePage = templateData => {
+// module.exports = templateData => {
+    // console.log(templateData);
+
+    //destructure projects and about data from templateData based on their property key names
+    const { projects, about, ...header } = templateData;
+    
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -34,11 +40,29 @@ export const generatePage = (userName, githubName) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-eqiv="X=UA-Compatible" content="ie=edge">
             <title>Portfolio</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+            <link href="https://fonts.googleapis.cm/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="style.css" />
+
         </head>
 
         <body>
-            <h1>${userName}</h1>
-            <h2><a href="https://github.com/${githubName}">Github</a></h2>
+            <header>
+                <div class="container flex-row justify-space-between align-center py-3>
+                    <h1 class="page-title text-secondary bd-dark py-2 px-3">${header.name}</h1>
+                    <nav class="flex-row">
+                        <a class="ml-2 my-1 px-2 py-1 bg-seconday text-dark" href="https://github.com/${
+                            header.github
+                        }">GitHub</a>
+                    </nav>
+                </div>
+            </header>
+            <main class-"container my-5">
+
+            </main>
+            <footer class="container text-center py-3">
+                <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+            </footer>
         </body>
     </html>
     `;
